571df7b3c6524e1d5d709ff375def817e4a324a7
Gareth <garethwhitleychard@gmail.com>
2025-02-26 21:09:57 +0000

    chore: rearrange port constant for heroku

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const server = express();
 const app = require('./app');
 const api = require('./api');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 server.use('/', app);
 server.use('/api', api);
 
 server.listen(PORT, () => {
-    console.log('Server is running and has made /api available for the API and / available for the app on port' + PORT);
+    console.log('Server is running and has made /api available for the API and / available for the app on port ' + PORT);
 });
