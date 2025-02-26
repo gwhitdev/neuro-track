741a980db77d1f3b46655c2ae9f126136fcc049c
Gareth <garethwhitleychard@gmail.com>
2025-02-26 21:06:25 +0000

    chore: update port on for heroku

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const server = express();
 const app = require('./app');
 const api = require('./api');
+const PORT = 3000 || process.env.port;
+
 
 server.use('/', app);
 server.use('/api', api);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('Server is running and has made /api available for the API and / available for the app on port 3000');
 });
