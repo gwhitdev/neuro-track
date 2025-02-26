b6ac0e221e8ad16bbce324844065036c9936c201
Gareth <garethwhitleychard@gmail.com>
2025-02-26 23:39:36 +0000

    fix: set path to static public directory

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT;
 const ENVIRONMENT = process.env.ENVIRONMENT;
 const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
