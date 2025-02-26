b3af6a152995b858610cdb62f487425f92a23328
Gareth <garethwhitleychard@gmail.com>
2025-02-26 21:01:24 +0000

    chore: add package.json files

--- api/api.js
+++ api/api.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const api = express();
+const PORT = process.env.PORT;
+const ENVIRONMENT = process.env.ENVIRONMENT;
 
 api.get('/', (req, res) => {
   console.log('API GET request received');
   res.send('hi API');
 });
+
+if (ENVIRONMENT === "dev") {
+  api.listen(PORT, () => {
+    console.log('API is running on port ' + PORT);
+  });
+}
 
 module.exports = api;
--- app/app.js
+++ app/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express();
+const PORT = process.env.PORT;
+const ENVIRONMENT = process.env.ENVIRONMENT;
 
 app.get('/', (req, res) => {
   console.log('APP GET request received');
   res.send('hi APP');
 });
+
+if (ENVIRONMENT === "dev") {
+  app.listen(PORT, () => {
+    console.log('APP is running on port ' + PORT);
+  });
+}
 
 
 module.exports = app;
