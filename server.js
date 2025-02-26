e6bca82d85abdb1cea4640b3708f79a2758b96a3
Gareth <garethwhitleychard@gmail.com>
2025-02-26 21:07:09 +0000

    chore: update for new port constant

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ server.use('/', app);
 server.use('/api', api);
 
 server.listen(PORT, () => {
-    console.log('Server is running and has made /api available for the API and / available for the app on port 3000');
+    console.log('Server is running and has made /api available for the API and / available for the app on port' + PORT);
 });
