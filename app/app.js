5412ccf593e7f6bb281d3d7465ea2930553f9983
Gareth <garethwhitleychard@gmail.com>
2025-02-26 23:37:09 +0000

    chore: set views path

--- app/app.js
+++ app/app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT;
 const ENVIRONMENT = process.env.ENVIRONMENT;
+const path = require('path');
 
 app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
