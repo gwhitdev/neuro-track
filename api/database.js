f165299e6da71c436be2850f61aae96edf7d96d7
Gareth <garethwhitleychard@gmail.com>
2025-02-27 14:08:27 +0000

    chore: remove redundant code

--- api/database.js
+++ api/database.js
@@ -1,8 +1,8 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    username: process.env.REDIS_USR,// || 'neurotrack_app_api_dev',
-    password: process.env.REDIS_PWD,// || 'CariadWren2028!',
+    username: process.env.REDIS_USR,
+    password: process.env.REDIS_PWD,
     socket: {
         host: 'redis-15680.c338.eu-west-2-1.ec2.redns.redis-cloud.com',
         port: 15680
@@ -12,7 +12,5 @@ const client = createClient({
 client.on('error', err => console.error('Redis client error', err));
 
 await client.connect();
-await client.set('foo','bar');
-const result = await client.get('foo');
 
-export { client, result };
+export { client };
