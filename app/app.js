bd9351d259e27fa757592044992cec2a48321e36
Gareth <garethwhitleychard@gmail.com>
2025-02-27 09:44:25 +0000

    feat: add auth0 to app

--- app/app.js
+++ app/app.js
@@ -3,7 +3,19 @@ const app = express();
 const PORT = process.env.PORT;
 const ENVIRONMENT = process.env.ENVIRONMENT;
 const path = require('path');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0SECRET,
+  baseURL: 'http://localhost:4002',
+  clientID: '5cO4WcJMs86IVjRYKhOfm35KlW0yJBdy',
+  issuerBaseURL: 'https://dev-3-d1xtuk.eu.auth0.com'
+};
+
+app.use(auth(config));
 app.use(express.static(path.join(__dirname,'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,11 +24,21 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/callback', (req, res) => {
+  res.redirect('/dashboard');
+})
+
+app.get('/dashboard', requiresAuth(), (req, res) => {
+  const data = {
+    user: JSON.stringify(req.oidc.user)
+  }
+  res.render('dashboard', data);
+})
+
 if (ENVIRONMENT === "dev") {
   app.listen(PORT, () => {
     console.log('APP is running on port ' + PORT);
   });
 }
-
 
 module.exports = app;
