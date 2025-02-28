d030607ffce38160403d142b69f71b93a7a62d44
Gareth <garethwhitleychard@gmail.com>
2025-02-28 00:10:29 +0000

    feat: add timestamp to sort by in redisearch

--- api/api.js
+++ api/api.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import { urlencoded, json } from "express";
 const api = express();
-const PORT = process.env.PORT;
-const ENVIRONMENT = process.env.ENVIRONMENT;
+import crypto from 'crypto';
 const {client} = await import('./database.js')
 api.use(express.urlencoded({extended: true}));
 api.use(express.json());
@@ -20,35 +18,58 @@ api.get('/test-redis', (req, res) => {
   })
 })
 
-const recordNumber = Math.floor(Math.random()*100);
+const recordNumber = (usr,date) => crypto.createHash('sha256').update(`${usr}${date}${Math.random()*1000}`).digest('base64');
 const date = new Date();
 const humanDate = date.toDateString();
-function createRecord(sub) {
 
-  client.hSet(`record:${recordNumber}`,{
+async function createRecord(sub) {
+
+  const num = recordNumber(sub,humanDate);
+  console.log('record number: ', num)
+  return await client.hSet(`record:${num}`,{
     user: sub,
     indicatorType: "memory",
     date: `${humanDate}`,
-    notes: "This is a space to put some notes"
+    notes: "This is a space to put some notes",
+    timestamp: Date.now()
   })
 }
 
 api.get('/records', async (req, res) => {
-  const records = await client.ft.search('usrname',req.query.usr);
-  if (records) {
-    const data = [
-          {
-            records,
-            recordNumber
-          }
-        ]
-        res.json(data)
+  try {
+    console.log('Searching for records for user: ', req.query.usr);
+    const records = await client.ft.search('index_with_timestamp',req.query.usr,{
+      SORTBY: {
+        BY: 'timestamp',
+        DIRECTION: 'DESC'
+      },
+      LIMIT: {
+        from:0, size:100
+      }
+    });
+    if (records) {
+      const data = [
+        {
+          records
+        }
+      ]
+      res.json(data)
+    }
+  } catch(err) {
+    console.error(err)
   }
+
 })
 
-api.post('/record', (req, res) => {
-  createRecord(req.body.user);
-  console.log(req.body.user)
+api.post('/record', async (req, res) => {
+try {
+  console.log('Trying to create a record for user:',req.body.user);
+
+ const created = await createRecord(req.body.user);
+ console.log(created)
+} catch (err) {
+  console.error(err);
+}
   res.json(
       {"body": req.body.user}
   )
--- app/public/assets/js/dashboard.js
+++ app/public/assets/js/dashboard.js
@@ -1,48 +1,30 @@
-const btn = document.getElementById('create');
 const myData = {};
-
-btn.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    fetch('api/record', {
-        method: 'POST',
-        body: JSON.stringify({"user": btn.dataset.sub}),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json())
-        .catch(err => console.error(err))
-})
+const generatedElement = {element: null};
+const loading = {dataLoading: false};
 
 const getDataButton = document.getElementById('getData');
-getDataButton.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    fetch(`api/records?usr=${getDataButton.dataset.sub}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data[0].records.documents)
-            myData.data = data[0].records.documents;
-            loadDataToDom()
-        })
-        .catch(err => console.error(err));
-});
-
+const btn = document.getElementById('create');
 const dataDiv = document.getElementById('data');
+
+if (loading.dataLoading === true) {
+    const text = document.createTextNode('Loading');
+    dataDiv.appendChild(text);
+}
+
+function clearCurrentDom() {
+    if (generatedElement.element instanceof HTMLElement) {
+        const div = document.getElementById('data');
+        div.removeChild(generatedElement.element);
+        myData.data = null;
+    }
+}
 
 function createRandomKey() {
     return Math.floor(Math.random()*1000).toString();
 }
 
-const generatedElement = {element: null};
-
-function clearCurrentDom() {
-    const div = document.getElementById('data');
-    div.removeChild(generatedElement.element);
-}
 function loadDataToDom() {
-    if (myData.data.length > 0) {
+    if (myData.data instanceof Array && myData.data.length > 0) {
         const list = document.createElement('ol');
         const nodes = [];
         const listKey = createRandomKey();
@@ -53,7 +35,8 @@ function loadDataToDom() {
             nodes.push({
                 date: document.createTextNode(i.value.date),
                 type: document.createTextNode(i.value.indicatorType),
-                notes: document.createTextNode(i.value.notes)
+                notes: document.createTextNode(i.value.notes),
+                timestamp: document.createTextNode(i.value.timestamp)
             })
         })
 
@@ -66,15 +49,46 @@ function loadDataToDom() {
             li.appendChild(n.date);
             const detailsUl = document.createElement('ul');
             const type = createLi();
-            type.appendChild(n.type);
             const notes = createLi();
+            const timestamp = createLi();
+            timestamp.appendChild(n.timestamp);
+            type.appendChild(n.type);
             notes.appendChild(n.notes);
+            detailsUl.appendChild(timestamp);
             detailsUl.appendChild(type);
             detailsUl.appendChild(notes)
             li.appendChild(detailsUl);
             list.appendChild(li);
         })
         dataDiv.append(list)
-
     }
 }
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetch('api/record', {
+        method: 'POST',
+        body: JSON.stringify({"user": btn.dataset.sub}),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .catch(err => console.error(err))
+})
+
+getDataButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    loading.dataLoading = true;
+    fetch(`api/records?usr=${getDataButton.dataset.sub}`)
+        .then(response => response.json())
+        .then(data => {
+            clearCurrentDom()
+            console.log(data[0].records.documents)
+            myData.data = data[0].records.documents;
+            loadDataToDom()
+
+        })
+        .then(() => loading.dataLoading = true)
+        .catch(err => console.error(err));
+});
